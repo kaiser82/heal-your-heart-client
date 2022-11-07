18a0481828ccd948adfea072c735d69fcb5af931
kaiser82 <dr.kaiserahmed@gmail.com>
2022-11-08 01:51:43 +0600

    header and footer added

---
+++ src/Routes/Routes.js
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
+import Home from "../components/Home/Home";
+import Main from "../Layouts/Main";
+
+
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+        ]
+    }
+])
